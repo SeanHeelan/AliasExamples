ee8fbbc22b35e0b90c0c1d6ed0b48029a17aebf8
Sean Heelan <sean@optimyze.cloud>
2021-11-04 20:58:36 +0000

    Formatting

--- negative.cpp
+++ negative.cpp
@@ -1,15 +1,16 @@
 // This file contains examples that do NOT have the sorts of aliasing issues
 // that force the compiler to generate extra loads from memory.
 
-#include <vector>
-#include <cstdint>
 #include <stddef.h>
+
+#include <cstdint>
+#include <vector>
 
 #include "aetypes.h"
 
 // From https://travisdowns.github.io/blog/2019/08/26/vector-inc.html
 // No aliasing issues as the vector is of type uint32_t
-void vector32_inc(std::vector<uint32_t>& v) {
+void vector32_inc(std::vector<uint32_t> &v) {
     for (size_t i = 0; i < v.size(); i++) {
         v[i]++;
     }
@@ -18,19 +19,17 @@ void vector32_inc(std::vector<uint32_t>& v) {
 // From PHP ext/fileinfo/libmagic/cdf.c
 // No aliasing issues as `buf`, `len` and `p` are passed on the stack and the
 // write through `buf` cannot alias any relevant data
-char * cdf_u16tos8(char *buf, size_t len, const uint16_t *p)
-{
-        size_t i;
-        for (i = 0; i < len && p[i]; i++)
-                buf[i] = (char)p[i];
-        buf[i] = '\0';
-        return buf;
+char *cdf_u16tos8(char *buf, size_t len, const uint16_t *p) {
+    size_t i;
+    for (i = 0; i < len && p[i]; i++) buf[i] = (char)p[i];
+    buf[i] = '\0';
+    return buf;
 }
 
 // Inspired by K2 src/isa/ebpf/inst_var.cc
 // https://github.com/SeanHeelan/superopt/blob/407eb410466e1a21d081e96cc803ea6a59aa7e7e/src/isa/ebpf/inst_var.cc#L1251
 // There are no aliasing issues as the vectors are of uint32_t
-void init_safety_chk32(const std::vector<uint32_t>& reg_in, std::vector<uint32_t>& reg_out) {
+void init_safety_chk32(const std::vector<uint32_t> &reg_in, std::vector<uint32_t> &reg_out) {
     for (int i = 0; i < reg_out.size(); ++i) {
         reg_out[i] = reg_in[i];
     }
@@ -48,15 +47,14 @@ void write2d_noalias(char **buf, size_t x, size_t y) {
     }
 }
 
-// This function is different to its variant in the positive examples in that the 
-// write through the character pointer has been lifted out of the loop. As a result, 
+// This function is different to its variant in the positive examples in that the
+// write through the character pointer has been lifted out of the loop. As a result,
 // the compiler can generate code to load the int pointer once, then produce an optimised
 // loop body.
-void write_through_struct_noalias(char *buf, PointerHolder *ptr, int y, size_t cnt)
-{
+void write_through_struct_noalias(char *buf, PointerHolder *ptr, int y, size_t cnt) {
     *(ptr->x) = 0;
     buf[0] = 'A';
-    for (size_t i = 0; i < cnt; ++i) {        
+    for (size_t i = 0; i < cnt; ++i) {
         *(ptr->x) += i;
     }
 }
--- positive.cpp
+++ positive.cpp
@@ -1,23 +1,22 @@
 // This file contains examples that have the sorts of aliasing issues that force
 // the compiler to generate extra loads from memory.
 
+#include <stddef.h>
+
 #include <cstdint>
 #include <vector>
-#include <stddef.h>
 
 #include "aetypes.h"
 
 // From https://travisdowns.github.io/blog/2019/08/26/vector-inc.html
-void vector8_inc(std::vector<uint8_t> &v)
-{
+void vector8_inc(std::vector<uint8_t> &v) {
     for (size_t i = 0; i < v.size(); i++) {
         v[i]++;
     }
 }
 
 // As above, but char
-void vector8_inc(std::vector<char> &v)
-{
+void vector8_inc(std::vector<char> &v) {
     for (size_t i = 0; i < v.size(); i++) {
         v[i] = (char)((uint8_t)v[i] + 1);
     }
@@ -25,8 +24,7 @@ void vector8_inc(std::vector<char> &v)
 
 // Inspired by K2 src/isa/ebpf/inst_var.cc
 // https://github.com/SeanHeelan/superopt/blob/407eb410466e1a21d081e96cc803ea6a59aa7e7e/src/isa/ebpf/inst_var.cc#L1251
-void init_safety_chk(const std::vector<uint8_t> &reg_in, std::vector<uint8_t> &reg_out)
-{
+void init_safety_chk(const std::vector<uint8_t> &reg_in, std::vector<uint8_t> &reg_out) {
     for (int i = 0; i < reg_out.size(); ++i) {
         reg_out[i] = reg_in[i];
     }
@@ -38,8 +36,7 @@ void init_safety_chk(const std::vector<uint8_t> &reg_in, std::vector<uint8_t> &r
 // However, due to how std::vector is implemented operator[] and the size
 // function still require a dereference through the reg_in/reg_out variables in
 // order to access required data.
-void init_safety_chk_no_pointers(const std::vector<uint8_t> reg_in, std::vector<uint8_t> reg_out)
-{
+void init_safety_chk_no_pointers(const std::vector<uint8_t> reg_in, std::vector<uint8_t> reg_out) {
     for (int i = 0; i < reg_out.size(); ++i) {
         reg_out[i] = reg_in[i];
     }
@@ -47,8 +44,7 @@ void init_safety_chk_no_pointers(const std::vector<uint8_t> reg_in, std::vector<
 
 // The inner loop of this function has a write that forces the compiler to
 // generate code to reload the row pointers
-void write2d_alias(char **buf, size_t x, size_t y)
-{
+void write2d_alias(char **buf, size_t x, size_t y) {
     for (size_t i = 0; i < y; ++i) {
         for (size_t j = 0; j < x; ++j) {
             buf[i][j] = 0;
@@ -56,11 +52,10 @@ void write2d_alias(char **buf, size_t x, size_t y)
     }
 }
 
-// The inner loop of this function has a write through a pointer to an int. There is 
-// also a write through a char pointer meaning that the pointer to the int must be 
+// The inner loop of this function has a write through a pointer to an int. There is
+// also a write through a char pointer meaning that the pointer to the int must be
 // loaded on each iteration.
-void write_through_struct(char *buf, PointerHolder *ptr, int y, size_t cnt)
-{
+void write_through_struct(char *buf, PointerHolder *ptr, int y, size_t cnt) {
     *(ptr->x) = 0;
     for (size_t i = 0; i < cnt; ++i) {
         buf[i] = 'A';
