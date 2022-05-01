8aa9aeffb89600c7a2cfce44da6313f7d561730c
Sean Heelan <sean@optimyze.cloud>
2022-05-01 17:04:11 +0100

    Compiler explorer code

---
+++ queries/benchmarks/compiler_explorer/bitcoin_SwapBase64.cpp
@@ -0,0 +1,53 @@
+#include <string>
+
+std::string orig_SwapBase64(const std::string& from)
+{
+    std::string to;
+    to.resize(from.size());
+    for (size_t i = 0; i < from.size(); ++i) {
+        switch (from[i]) {
+        case '-':
+            to[i] = '+';
+            break;
+        case '~':
+            to[i] = '/';
+            break;
+        case '+':
+            to[i] = '-';
+            break;
+        case '/':
+            to[i] = '~';
+            break;
+        default:
+            to[i] = from[i];
+            break;
+        }
+    }
+    return to;
+}
+
+std::u8string opt_SwapBase64(const std::u8string& from)
+{
+    std::u8string to;
+    to.resize(from.size());
+    for (size_t i = 0; i < from.size(); ++i) {
+        switch (from[i]) {
+        case '-':
+            to[i] = '+';
+            break;
+        case '~':
+            to[i] = '/';
+            break;
+        case '+':
+            to[i] = '-';
+            break;
+        case '/':
+            to[i] = '~';
+            break;
+        default:
+            to[i] = from[i];
+            break;
+        }
+    }
+    return to;
+}
