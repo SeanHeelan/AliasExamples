14cd2d1ac0c24f9d89d61818005c2206948e5af1
Sean Heelan <sean@optimyze.cloud>
2022-05-01 17:02:03 +0100

    Revert to original argument passing

--- queries/benchmarks/bitcoin_SwapBase64.cc
+++ queries/benchmarks/bitcoin_SwapBase64.cc
@@ -3,34 +3,9 @@
 
 #include <vector>
 
-std::string orig_SwapBase64(const std::string& from, std::string& to)
+std::string orig_SwapBase64(const std::string& from)
 {
-    for (size_t i = 0; i < from.size(); ++i) {
-        switch (from[i]) {
-        case '-':
-            to[i] = '+';
-            break;
-        case '~':
-            to[i] = '/';
-            break;
-        case '+':
-            to[i] = '-';
-            break;
-        case '/':
-            to[i] = '~';
-            break;
-        default:
-            to[i] = from[i];
-            break;
-        }
-    }
-    return to;
-}
-
-/*
-std::vector<uint8_t> orig_vec(const std::string& from)
-{
-    std::vector<uint8_t> to;
+    std::string to;
     to.resize(from.size());
     for (size_t i = 0; i < from.size(); ++i) {
         switch (from[i]) {
@@ -53,10 +28,11 @@ std::vector<uint8_t> orig_vec(const std::string& from)
     }
     return to;
 }
-*/
 
-std::u8string opt_SwapBase64(const std::u8string& from, std::u8string& to)
+std::u8string opt_SwapBase64(const std::u8string& from)
 {
+    std::u8string to;
+    to.resize(from.size());
     for (size_t i = 0; i < from.size(); ++i) {
         switch (from[i]) {
         case '-':
@@ -81,11 +57,9 @@ std::u8string opt_SwapBase64(const std::u8string& from, std::u8string& to)
 
 static void optimised(benchmark::State& state) {
   auto s = std::u8string(state.range(0), 'A');
-  std::u8string to;
-  to.resize(8<<21);
   // Code inside this loop is measured repeatedly
   for (auto _ : state) {
-    auto r = opt_SwapBase64(s, to);
+    auto r = opt_SwapBase64(s);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(r);
   }
@@ -95,11 +69,9 @@ BENCHMARK(optimised)->RangeMultiplier(2)->Range(8, 8<<21);
 
 static void original(benchmark::State& state) {
   auto s = std::string(state.range(0), 'A');
-  std::string to;
-  to.resize(8<<21);
   // Code inside this loop is measured repeatedly
   for (auto _ : state) {
-    auto r = orig_SwapBase64(s, to);
+    auto r = orig_SwapBase64(s);
     // Make sure the variable is not optimized away by compiler
     benchmark::DoNotOptimize(r);
   }
